5d99309bda748fe7cfe07eed594d90511a471752
lenmorld <lenmorld@gmail.com>
2019-02-12 00:34:28 -0500

    callback version

--- mongo_db.js
+++ mongo_db.js
@@ -2,19 +2,33 @@
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
-function init_db(db_connection_url) {
-    // function returns a promise that resolves to a mongodb instance
-    return new Promise(function(resolve, reject) {
-        MongoClient.connect(db_connection_url, function(err, db_instance) {
-            if (err) {
-                console.log(`[MongoDB connection] ERROR: ${err}`);
-                reject(err);        // this should be "caught" by the calling function
-            }
-
+// CALLBACK VERSION
+function init_db(db_connection_url, successCallback, failureCallback) {
+    MongoClient.connect(db_connection_url, function(err, db_instance) {
+        if (err) {
+            console.log(`[MongoDB connection] ERROR: ${err}`);
+            failureCallback(err);        // this should be "caught" by the calling function
+        } else {
             console.log("[MongoDB connection] SUCCESS");
-            resolve(db_instance);
-        });
+            successCallback(db_instance);
+        }
     });
 }
+
+// PROMISE VERSION
+// function init_db(db_connection_url) {
+//     // function returns a promise that resolves to a mongodb instance
+//     return new Promise(function(resolve, reject) {
+//         MongoClient.connect(db_connection_url, function(err, db_instance) {
+//             if (err) {
+//                 console.log(`[MongoDB connection] ERROR: ${err}`);
+//                 reject(err);        // this should be "caught" by the calling function
+//             }
+
+//             console.log("[MongoDB connection] SUCCESS");
+//             resolve(db_instance);
+//         });
+//     });
+// }
 
 module.exports = { init_db };
